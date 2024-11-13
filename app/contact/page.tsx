bc951fee4274a68338f53bbdcd4611fe2a700c97
nafeyuldin1 <lasthwwy@gmail.com>
2024-11-13 15:50:51 -0500

    files update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -102,7 +102,10 @@ export default function Contact() {
               placeholder='Enter your message (optional) ' 
             />
           </div>
-
+          <div id="recaptcha" className="g-recaptcha"
+     
+     data-sitekey="6Lf-130qAAAAAGh8qfnWKlUvWTv0ZEQuzOefqvsx"
+     ></div>
           <button type="submit" className={`bg-black font-bold hover:shadow-lg hover:shadow-green-400 transition-shadow duration-500 text-lg my-4 md:text-2xl text-white py-2 px-10 min-w-fit w-fit rounded-md mx-auto ${SedaN} `}>Submit</button>
         </form>
       </div>
